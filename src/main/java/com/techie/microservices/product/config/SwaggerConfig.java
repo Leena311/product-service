3e1caeb18bb953027fa8618e18975db92a28eff5
Madhusmita <info.smita2021@gmail.com>
2025-08-14 01:46:53 +0530

    properties file updated

--- src/main/java/com/techie/microservices/product/config/SwaggerConfig.java
+++ src/main/java/com/techie/microservices/product/config/SwaggerConfig.java
@@ -1,31 +1,31 @@
-package com.techie.microservices.product.config;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import java.util.List;
-
-@Configuration
-public class SwaggerConfig {
-
-    @Bean
-    @Primary
-    public OpenAPI OpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("Product Service API").version("1.0"))
-                // .servers(List.of(new Server().url("http://localhost:9000/aggregate/inventory-service")))
-                .servers(List.of(new Server().url("http://localhost:9000")))
-                .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
-                .components(new Components()
-                        .addSecuritySchemes("basicAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("basic")));
-    }
-}
+//package com.techie.microservices.product.config;
+//import io.swagger.v3.oas.models.Components;
+//import io.swagger.v3.oas.models.OpenAPI;
+//import io.swagger.v3.oas.models.info.Info;
+//import io.swagger.v3.oas.models.security.SecurityRequirement;
+//import io.swagger.v3.oas.models.security.SecurityScheme;
+//import io.swagger.v3.oas.models.servers.Server;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//
+//import java.util.List;
+//
+//@Configuration
+//public class SwaggerConfig {
+//
+//    @Bean
+//    @Primary
+//    public OpenAPI OpenAPI() {
+//        return new OpenAPI()
+//                .info(new Info().title("Product Service API").version("1.0"))
+//                // .servers(List.of(new Server().url("http://localhost:9000/aggregate/inventory-service")))
+//                .servers(List.of(new Server().url("http://localhost:9000")))
+//                .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
+//                .components(new Components()
+//                        .addSecuritySchemes("basicAuth",
+//                                new SecurityScheme()
+//                                        .type(SecurityScheme.Type.HTTP)
+//                                        .scheme("basic")));
+//    }
+//}
